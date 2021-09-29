42151e7a349e053b6d38502edc9f582101f19c3f
Godwin ELITCHA <godwin.elitcha@gmail.com>
2021-09-29 16:14:27 +0000

    Initial commit

---
+++ acf-to-api.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+  Plugin Name: ACF TO API
+  Plugin URI: https://godwinelitcha.com/
+  Description: Add data from Advanced Custom Fields to the Wordpress REST API.
+  Version: 1.0.0
+  Author: Godwin ELITCHA
+ *
+ */
+
+add_action('rest_api_init', 'acf2api_hook_all_post_types', 99);
+function acf2api_hook_all_post_types()
+{
+
+  //Get all the post types
+  global $wp_post_types;
+  $post_types = array_keys($wp_post_types);
+
+  //Loop through each one
+  foreach ($post_types as $post_type) {
+
+    //Add a filter for this post type
+    add_filter('rest_prepare_' . $post_type, function ($data, $post, $request) {
+
+      //Get the response data
+      $response_data = $data->get_data();
+
+      //Bail early if there's an error
+      if ($request['context'] !== 'view' || is_wp_error($data)) {
+        return $data;
+      }
+
+      //Get all fields
+      $fields = get_fields($post->ID);
+      //If we have fields...
+      if ($fields) {
+        //Loop through them...
+        foreach ($fields as $field_name => $value) {
+          //Set the meta
+          $response_data[$field_name] = $value;
+        }
+      }
+
+      //Commit the API result var to the API endpoint
+      $data->set_data($response_data);
+      return $data;
+    }, 10, 3);
+  }
+}
